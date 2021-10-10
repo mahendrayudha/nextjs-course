8bde92d67de9109ee1ba3598ce53e28bab702590
Octavian Yudha Mahendra <54369399+mahendrayudha@users.noreply.github.com>
2021-10-10 13:54:47 +0700

    Navigating to Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "octavian", name: "Octavian Yudha" },
+    { id: "mahendra", name: "Mahendra Yudha" },
+    { id: "yudha", name: "Yudha Octavian" },
+  ];
+
   return (
     <div>
       <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
